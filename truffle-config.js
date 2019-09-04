358ff5b6a193a32d8fabd18e1bdfa7b28501f6a0
Harry Liu <harry.liu@cyberrepublic.org>
2019-09-04 16:35:31 +0800

    clean the code

--- truffle-config.js
+++ truffle-config.js
@@ -3,20 +3,18 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   compilers: {
     solc: {
       version: "0.5.1"
     }
   },
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
       host: "localhost",
-      // port: 8545
       port: 20636,
       gas: 8000000,
-      // gasPrice: web3.utils.toWei("5", "gwei"),
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
     elaeth: {
       host: "https://rpc.elaeth.io",
