33796cb07ea0c607f36cbedabcc6290fd562f8cc
Harry Liu <harry.liu@cyberrepublic.org>
2019-09-04 18:22:29 +0800

    improved code

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import getWeb3 from "./utils/getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, value: 0 };
 
   componentDidMount = async () => {
     try {
@@ -25,7 +25,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,11 +35,11 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  onSubmit = async () => {
+    const { accounts, contract, value } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(55).send({ from: accounts[0] });
+    await contract.methods.set(value).send({ from: accounts[0] });
     // await contract.methods.set(5).send({ from: "0x8f4E659b586A0d279BA674351C26fBcf2e55a5C3" });
     console.log('accounts are: ', accounts)
     // Get the value from the contract to prove it worked.
@@ -58,16 +58,20 @@ class App extends Component {
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
         <div>The stored value is: {this.state.storageValue}</div>
+        <p>
+          <input onChange={this.onChange} />
+        </p>
+        <p>
+          <button onClick={this.onSubmit}>Submit</button>
+        </p>
       </div>
     );
+  }
+
+  onChange = (e) => {
+    console.log('value: ', e.target.value)
+    this.setState({ value: e.target.value })
   }
 }
 
